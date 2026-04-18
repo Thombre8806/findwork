ecc2e86e206eb7b56d57cdaef72b03f5d9dc8483
Thombre8806 <omthombre88@gmail.com>
2026-04-18 10:19:48 +0530

    fix: change postcss config to standard

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
-// postcss.config.js
 export default {
   plugins: {
-    "@tailwindcss/postcss": {}, // Tailwind v4 साठी हे नाव वापरणे अनिवार्य आहे
-    autoprefixer: {},           // वेगवेगळ्या ब्राउझरमध्ये CSS नीट चालण्यासाठी
+    tailwindcss: {},
+    autoprefixer: {},
   },
 }
